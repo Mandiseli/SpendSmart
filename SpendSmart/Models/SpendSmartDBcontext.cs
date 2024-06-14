bda8e09e838df3f3854aed73ab8d7de78d19ba13
mandiseli <mandiseli@outlook.com>
2024-06-14 17:49:21 +0200

    Add project files.

---
+++ SpendSmart/Models/SpendSmartDBcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDBcontext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDBcontext(DbContextOptions<SpendSmartDBcontext> options) 
+            : base(options)
+        {
+            
+        }
+    }
+}
